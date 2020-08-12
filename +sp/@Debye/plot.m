7cb0b043cd31250460a34b9d5d08974d5fd07c8b
jhilgar <jhilgar@ucsd.edu>
2020-08-11 18:30:54 -0700

    Update plot@sp.Debye with arrhenius plotting

--- +sp/@Debye/plot.m
+++ +sp/@Debye/plot.m
@@ -1,30 +1,32 @@
 function plot(obj, plot_type, varargin)
+    if isempty(obj.model_data)
+        warning('data is not fit');
+    else
+        group = obj.model_data.TemperatureRounded;
+        switch plot_type
+            case 'in'
+                xmodel = obj.model_data.Frequency;
+                ymodel = obj.model_data.ChiIn;
+            case 'out'
+                xmodel = obj.model_data.Frequency;
+                ymodel = obj.model_data.ChiOut;
+            case 'cole'
+                xmodel = obj.model_data.ChiIn;
+                ymodel = obj.model_data.ChiOut;
+            case 'arrhenius'
+                group = obj.fits.TemperatureRounded;
+                xmodel = 1 ./ obj.fits.TemperatureRounded;
+                ymodel = log(obj.fits.cc_tau_1);
+                sp.PlotHelper.scatter(xmodel, ymodel, group, 'd');
+                return
+            otherwise
+                error('unsupported plot type');
+        end
+        sp.PlotHelper.plot(xmodel, ymodel, group);
+    end
+    
     for a = 1:length(obj.datafiles)
         obj.datafiles(a).plot(plot_type, varargin{:});
     end
     sp.PlotHelper.set_impedence_axes(plot_type);
-
-    if isempty(obj.model_data)
-        sp.PlotHelper.make_pretty(2, 10, 1);
-        warning('data is not fit');
-        return
-    end
-    
-    group = obj.model_data.TemperatureRounded;
-    switch plot_type
-        case 'in'
-            xmodel = obj.model_data.Frequency;
-            ymodel = obj.model_data.ChiIn;
-        case 'out'
-            xmodel = obj.model_data.Frequency;
-            ymodel = obj.model_data.ChiOut;
-        case 'cole'
-            xmodel = obj.model_data.ChiIn;
-            ymodel = obj.model_data.ChiOut;
-        otherwise
-            error('unsupported plot type');
-    end
-
-    sp.PlotHelper.plot(xmodel, ymodel, group);
-    sp.PlotHelper.make_pretty(2, 10, 1);
 end
